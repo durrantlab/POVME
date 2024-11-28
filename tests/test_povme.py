cf3f6db29f135bf7c4b54b386b2c59612aa721be
Alex Maldonado <alex@oasci.org>
2024-11-28 08:17:43 -0500

    tests: fix

--- tests/test_povme.py
+++ tests/test_povme.py
@@ -19,13 +19,7 @@ def test_4nss(path_4nss_input, path_4nss_output):
 
     expected_vols = set([1673.0, 1493.0, 1711.0, 1854.0, 2023.0])
     actual_vols = set(results.values())
-    if len(actual_vols - expected_vols) > 0:
-        raise Exception(
-            "Expected volumes to be "
-            + str(expected_vols)
-            + ", but got "
-            + str(actual_vols)
-        )
+    assert actual_vols == expected_vols
     num_output_files = len(glob.glob(dir_output + "/*"))
     if num_output_files != 12:
         raise Exception("Expected 12 output files, but got " + str(num_output_files))
